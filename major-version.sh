e4f2dfae127cde26ed3fde8a9545cc248a4c7a9a
WaterLemons2k <62788816+WaterLemons2k@users.noreply.github.com>
2023-02-22 14:14:06 +0800

    feat(major-version): use `-f` option to create and push tags
    Bump to v1.0.1

--- major-version.sh
+++ major-version.sh
@@ -5,7 +5,7 @@
 # BEFORE USING THIS SHELL, RUN `git add` AND `git commit`!
 # This shell is designed to automatically push version tag and MAJOR version tag.
 # Automatically detects if it is running under WSL. If so,
-# Use `powershell.exe` to run `git push`, Otherwize, use `sh`.
+# Use `powershell.exe` to run `git push`. Otherwize, use `sh`.
 # Usage: bash ./major-version.sh v1.0.0
 set -e
 
@@ -15,11 +15,14 @@ if [ -z $1 ]; then # If not exist $1
     exit 1
 fi
 
-echo "Auto push MAJOR version tag v1.0.0"
+echo "Auto push MAJOR version tag v1.0.1"
 VERSION=$1
 MAJOR=${VERSION%.*.*}
 echo "Version: $VERSION"
 echo "MAJOR version: $MAJOR"
+
+git tag $VERSION # Create a version tag
+git tag -f $MAJOR # Create a MAJOR version tag
 
 if [ -z $WSL_DISTRO_NAME ]; then # If the environment WSL_DISTRO_NAME does not exist (not WSL)
     PUSH="git push origin"
@@ -27,12 +30,4 @@ else # The environment WSL_DISTRO_NAME exists (is WSL)
     PUSH="powershell.exe git push origin"
 fi
 
-git tag $VERSION # Create a version tag
-
-if [ $(git tag -l $MAJOR) ]; then #If the MAJOR version tag exists
-    git tag -d $MAJOR # Delete the old MAJOR version tag
-    $PUSH :$MAJOR # Delete the MAJOR version tag from the remote
-fi
-
-git tag $MAJOR # Create a new MAJOR version tag
-$PUSH main $VERSION $MAJOR --atomic
+$PUSH main $VERSION $MAJOR --atomic -f
