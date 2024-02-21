6906d8a83b250adf755b409a8b75842db8f3220e
WaterLemons2k <62788816+WaterLemons2k@users.noreply.github.com>
2024-02-21 21:47:22 +0800

    chore(exec_url): delete temp file on exit

--- include/exec_url.sh
+++ include/exec_url.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
-curl --location --silent --output /tmp/script.sh "$URL" && bash /tmp/script.sh
-rm /tmp/script.sh
+TEMP_SCRIPT="/tmp/$(tr </dev/urandom -dc _a-z | head -c 10).sh"
+trap 'rm -f $TEMP_SCRIPT' EXIT
+
+curl --location --silent --output "$TEMP_SCRIPT" "$URL" && bash "$TEMP_SCRIPT"
